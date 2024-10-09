98915d178000ebe269f2b0d4b683ad7aff7fc50c
Stephan Model <stephan.model@outlook.de>
2024-10-09 14:44:32 +0200

    rework the JSX on <form> in Color form comp

--- src/App.jsx
+++ src/App.jsx
@@ -18,3 +18,37 @@ function App() {
 }
 
 export default App;
+
+/*
+
+#1
+
+✅ 1. Create ColorForm comp 
+
+✅ 2. Implement acc criteria 1,2 into ColorForm comp ✅
+
+3. Implement eventHandling logic
+
+Color Form >form submission data >> App >take form submission data >> App >> update > State
+
+3.1 we need to PASS FORM SUBMISSON data ColorForm to App
+add handleSubmit to ColorForm to capture form data
+
+3.2. set props 
+  xx> in ColorForm { onAddColor } return(<form onAddColor=handleSubmit) ❌ needs to be linked with handleSubmit
+  in ColorForm { onAddColor } handleSubmit(onAddColor(data))
+  in App fn handleAddColor return(<ColorForm onAddColor=handleAddColor)
+    => to enable communication of form submit data from Child to Parent
+
+3.3. we need a STATE for that, becuase values are going to change
+  we want the state to hold the [inital colors] for now
+  we want state management to add a color = new object to THE BEGINNING of an array (=> array .unhsift() )
+we need STATE MANAGEMENT in App, because data managment shall be handled there
+
+
+
+
+
+
+
+*/
--- src/Components/ColorForm/ColorForm.jsx
+++ src/Components/ColorForm/ColorForm.jsx
@@ -1,13 +1,25 @@
 import "./ColorForm.css";
 
-export default function ColorForm() {
+export default function ColorForm({ onAddColor }) {
+  // add fn to handle submit and store input data to be passed to App
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    console.log(data);
+
+    //
+  }
+
   return (
-    <form className="ColorForm--form">
+    <form className="ColorForm--form" onSubmit={handleSubmit}>
       <fieldset className="ColorForm--form--fieldset">
         <legend>Role</legend>
         <label htmlFor="color-role--input"></label>
         <input
           id="color-role--input"
+          name="role"
           type="text"
           placeholder="some color"
         ></input>
@@ -15,20 +27,26 @@ export default function ColorForm() {
       <fieldset className="ColorForm--form--fieldset">
         <legend>Hex</legend>
         <label htmlFor="color-hex--input"></label>
-        <input id="color-role--input" type="text" placeholder="#54c73d"></input>
-        <input id="color-role--input" type="color"></input>
+        <input
+          id="color-hex--input"
+          name="hex"
+          type="text"
+          placeholder="#54c73d"
+        ></input>
+        <input id="color-hex--input" type="color"></input>
       </fieldset>
       <fieldset className="ColorForm--form--fieldset">
         <legend>Contrast Text</legend>
         <label htmlFor="color-contrast-text--input"></label>
         <input
           id="color-contrast-text--input"
+          name="contrastText"
           type="text"
           placeholder="#54c73d"
         ></input>
-        <input id="color-contrast-text--input" type="color"></input>
+        <input name="color-contrast-text--input" type="color"></input>
       </fieldset>
-      <button type="button">ADD COLOR</button>
+      <button type="submit">ADD COLOR</button>
     </form>
   );
 }
