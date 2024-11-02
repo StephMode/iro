be184820f06c10219b4b58a033efd2ebe246afdb
Stephan Model <stephan.model@outlook.de>
2024-11-02 18:41:02 +0100

    clean up ColorEditor

--- src/Components/ColorEditor/ColorEditor.jsx
+++ src/Components/ColorEditor/ColorEditor.jsx
@@ -1,29 +1,16 @@
 import ColorInput from "../ColorInput/ColorInput";
 
 export default function ColorEditor({ currentColor, onEditColorSubmission }) {
-  // add fn to handle submit and store input data to be passed to App
   function handleSubmit(event) {
     event.preventDefault();
-    // store input data
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
-
-    // ensure that ID is kept with the updated color
-    // ...curentColor keeps the only value that's not overriden == id
-    // ...data overrides the data with the FormData
     const updatedColor = { ...currentColor, ...data };
     console.log(data);
 
     // to pass on the id + edited data as an object to the parents
     onEditColorSubmission(updatedColor);
-
-    // event handling logic to improve form UX
-    // 🟡 not sure if i need this at all
-    // event.target.reset();
-    // event.target.elements.role.focus();
   }
-
-  // need logic to make value of "role" equal to current role value of color in editing
 
   return (
     <form className="ColorForm--form" onSubmit={handleSubmit}>
