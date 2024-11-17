a893eb519d701d7e7b1dc3d9732afbd7aad713ba
Stephan Model <stephan.model@outlook.de>
2024-11-17 18:56:38 +0100

    change jsx in return of App to improve code legibility and nest .map of color cards in ul li for better semantics

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Color from "./Components/Color/Color";
 import ColorForm from "./Components/ColorForm/ColorForm";
 import { uid } from "uid";
 import useLocalStorageState from "use-local-storage-state";
+import ThemeSelector from "./Components/ThemeSelector/ThemeSelector";
 
 function App() {
   const [colors, setColors] = useLocalStorageState("colors", {
@@ -28,28 +29,32 @@ function App() {
   }
 
   return (
-    <>
+    <main>
       <h1>Theme Creator</h1>
+      <ThemeSelector></ThemeSelector>
 
       <ColorForm onAddColor={handleAddColor} />
-      {colors.length === 0 ? (
+      
+      {colors.length === 0 && (
         <h3 className="no-colors-message">
           🎨 No colors? How about adding one?
         </h3>
-      ) : (
-        ""
       )}
+      
+      <ul className="color-card--list">
       {colors.map((color) => {
         return (
+          <li key={color.id}>
           <Color
-            key={color.id}
             color={color}
             onDeleteColor={handleDeleteColor}
             onEditColor={handleEditColor}
           />
+          </li>
         );
       })}
-    </>
+      </ul>
+    </main>
   );
 }
 
---
+++ src/Components/ThemeSelector/ThemeSelector.jsx
@@ -0,0 +1,18 @@
+
+
+export default function ThemeSelector() {
+  return (
+    <>
+    <h2>ThemeSelector</h2>
+    <form>
+        <select>
+            <option>1</option>
+            <option>2</option>
+        </select>
+    </form>
+    <button>Add</button>
+    <button>Edit</button>
+    <button>Delete</button>
+    </>
+  )
+}
