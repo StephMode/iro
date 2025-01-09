7053e21c5a2e7a5637ae5059407b4445c3f462a2
Stephan Model <stephan.model@outlook.de>
2025-01-09 12:24:58 +0100

    implement logic and improve existing logic to handle theme change
    it was necessary to store the selectedTheme in a const with the value of the return value of a callback fn

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,38 @@ function App() {
   const [themes, setThemes] = useLocalStorageState("themes", {
     defaultValue: initialThemes,
   });
-  const [themeSelector, setThemeSelector] = useState(0);
+  // const [themeSelector, setThemeSelector] = useState(0);
+  const [selectedThemeId, setSelectedThemeId] = useState(initialThemes[0].id);
+  // const [coolors, setCoolors] = useState(selectedTheme.colors);
 
-  // TODO: this needs to be made dynamic
-  // let themeSelector = 0;
-  const selectedTheme = themes[themeSelector];
-  const selectedThemeColors = selectedTheme.colors;
+  // let selectedThemeColors = selectedTheme.colors; // FIXME: why not try a state or const instead of this
+  const selectedTheme = themes.find((t) => t.id === selectedThemeId);
 
-  
+  // console.log("This is the STATE selectedTheme:", selectedTheme);
+  // console.log("This is the LET selectedThemeColors:", selectedThemeColors);
+  // console.log("This is the CONST selectedThemeColors:", selectedThemeColors2);
+  // console.log("This is the STATE coolors:", coolors);
 
-  function selectTheme(id) {
-    setThemeSelector(id);
-    console.log(id)
-  }
+  // this returns `undefined`
+  // const selectedThemeColors2 = themes[selectedTheme];
+  // console.log("selThC2", selectedThemeColors2);
 
-  
-  
+  // useEffect(() => {
+  //   console.log("something changed with the colors of the selected theme");
+  //   selectedThemeColors = selectedTheme.colors;
+  // }, []);
+
+  // const yetAnotherThemeVar = themes[0].id;
+  // console.log("This is value of yetAnother:", yetAnotherThemeVar);
+
+  // function selectTheme(id) {
+  //   // const selTheme = themes.find((theme) => theme.id === id);
+  //   // const themeIndex = selTheme.id;
+  //   // setThemeSelector(id);
+  //   setSelectedTheme(themes.find((theme) => theme.id === id));
+  //   // console.log("This is the theme selected in option", selTheme);
+  //   console.log("This is the value of state selectedTheme now", selectedTheme);
+  // }
 
   function handleAddColor(newColor) {
     setThemes((prevThemes) =>
@@ -72,11 +88,11 @@ function App() {
   return (
     <main>
       <h1>Theme Creator</h1>
-      <ThemeSelector themes={themes} onThemeSelect={selectTheme}/>
+      <ThemeSelector themes={themes} onThemeSelect={setSelectedThemeId} />
 
       <ColorForm onAddColor={handleAddColor} />
 
-      {selectedThemeColors.length === 0 && (
+      {selectedTheme.length === 0 && (
         <h3 className="no-colors-message">
           🎨 No colors? How about adding one?
         </h3>
@@ -85,7 +101,7 @@ function App() {
       <h2>{selectedTheme.name}</h2>
 
       <ul className="color-card--list">
-        {selectedThemeColors.map((color) => {
+        {selectedTheme.colors.map((color) => {
           return (
             <li key={color.id}>
               <Color
--- src/Components/ThemeSelector/ThemeSelector.jsx
+++ src/Components/ThemeSelector/ThemeSelector.jsx
@@ -1,7 +1,7 @@
-export default function ThemeSelector({themes, onThemeSelect}) {
+export default function ThemeSelector({ themes, onThemeSelect }) {
   // TODO:
-  // 1) get rid of <form>
-  // 2) develop logic for theme selection
+  // 1) get rid of <form> ✅
+  // 2) develop logic for theme selection ✅
   // 3) develop edit theme logic -- take Color.jsx and App.jsx for color as an example
   // 4) develop delete theme logic -- take Color.jsx and App.jsx for color as an example
   // 5) develop add theme logic -- take Color.jsx and App.jsx for color as an example
@@ -14,18 +14,23 @@ export default function ThemeSelector({themes, onThemeSelect}) {
 
   const handleThemeSelect = (event) => {
     onThemeSelect(event.target.value);
-  }
+    console.log("The slected theme value", event.target.value);
+  };
 
   return (
     <>
       <h2>ThemeSelector</h2>
       <div style={{ display: "flex" }}>
-        {/* FIXME: select doesn't need a form */}
-        
         <select onChange={handleThemeSelect}>
-          {themes.map(theme => {return(<option key={theme.id} value={theme.id}>{theme.name}</option>)})}
+          {themes.map((theme) => {
+            return (
+              <option key={theme.id} value={theme.id}>
+                {theme.name}
+              </option>
+            );
+          })}
         </select>
-        
+
         <button>Add</button>
         <button>Edit</button>
         <button>Delete</button>
