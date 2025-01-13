c7288110465329f63cd663139f539344bd157b35
Stephan Model <stephan.model@outlook.de>
2025-01-13 20:35:38 +0100

    revert: delete statements for basic error handling
    this was the intial implementation of prop related error handling as implemented in 5655610, will be replaced by React propTypes

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,22 +1,4 @@
 export default function Button({ onClick, style, type }) {
-  const buttonTypes = ["add", "edit", "delete"];
-  const checkButtonType = buttonTypes.find((buttonType) => type === buttonType);
-
-  !type &&
-    console.warn(
-      "Button type missing. Provide one out of the valid types:",
-      buttonTypes
-    );
-
-  !onClick && console.warn("Please add onClick for button functionality");
-
-  checkButtonType === undefined &&
-    console.error(
-      type,
-      "is not a valid type. Provide one out of the valid types:",
-      buttonTypes
-    );
-
   return (
     <button onClick={onClick} style={style}>
       {type === "add" && "Add"}
