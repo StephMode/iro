5655610eca7b022e0b86b8b6b683e093a516f0b5
Stephan Model <stephan.model@outlook.de>
2025-01-13 11:05:57 +0100

    test: implement basic error handling for prop drilling
    implement error handling that features warning and error logs to the console when passing props incorrectly or not at all

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,7 +1,25 @@
-export default function Button({ onClick, style }) {
+export default function Button({ onClick, style, type }) {
+  const buttonTypes = ["add", "edit", "delete"];
+  const checkButtonType = buttonTypes.find((buttonType) => type === buttonType);
+
+  !type &&
+    console.warn(
+      "Button type missing. Provide one out of the valid types:",
+      buttonTypes
+    );
+
+  !onClick && console.warn("Please add onClick for button functionality");
+
+  checkButtonType === undefined &&
+    console.error(
+      type,
+      "is not a valid type. Provide one out of the valid types:",
+      buttonTypes
+    );
+
   return (
     <button onClick={onClick} style={style}>
-      Button
+      {type === "add" && "Add"}
     </button>
   );
 }
