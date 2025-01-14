f62ccb7f9772f3aa11f27d03fba9b96dc544b124
Stephan Model <stephan.model@outlook.de>
2025-01-14 16:48:51 +0100

    refactor: centralize button text rendering via object-based dictionary

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,14 +1,26 @@
 import PropTypes from "prop-types";
 
 export default function Button({ onClick, style, buttonType }) {
+  const buttonTypeLabes = {
+    labels: [
+      { labelType: "add", buttonLabel: "Add" },
+      { labelType: "edit", buttonLabel: "Edit" },
+      { labelType: "delete", buttonLabel: "Delete" },
+      { labelType: "save", buttonLabel: "Save" },
+      { labelType: "cancel", buttonLabel: "Cancel" },
+      { labelType: "copy", buttonLabel: "Copy" },
+    ],
+    returnLabel() {
+      const buttonLabelFinder = this.labels.find(
+        (label) => label.labelType === buttonType
+      );
+      return buttonLabelFinder ? buttonLabelFinder.buttonLabel : "";
+    },
+  };
+
   return (
     <button onClick={onClick} style={style}>
-      {buttonType === "add" && "Add"}
-      {buttonType === "edit" && "Edit"}
-      {buttonType === "delete" && "Delete"}
-      {buttonType === "save" && "Save"}
-      {buttonType === "cancel" && "Cancel"}
-      {buttonType === "copy" && "Copy"}
+      {buttonTypeLabes.returnLabel()}
     </button>
   );
 }
