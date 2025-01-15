ebbf1dded9d4d440a33c764b994a8b8500f4e7a4
Stephan Model <stephan.model@outlook.de>
2025-01-15 17:08:40 +0100

    refactor: rearrange jsx and components, update css and improve styling

--- src/Components/ColorCard/ColorCard.jsx
+++ src/Components/ColorCard/ColorCard.jsx
@@ -32,20 +32,22 @@ export default function ColorCard({ color, onDeleteColor, onEditColor }) {
         </div>
       ) : (
         <>
-          <h3 className="color-card-headline">{color.hex}</h3>
-          <CopyToClipboard hexValue={color.hex} />
+          <div>
+            <h3 className="color-card-headline">{color.hex}</h3>
 
-          <>
+            <h4>{color.role}</h4>
+            <p>contrast: {color.contrastText}</p>
+            <ContrastChecker color={color} />
+          </div>
+          <div className="color-card--buttons">
+            <CopyToClipboard hexValue={color.hex} />
+
             <Button
               buttonType="delete"
               onClick={() => onDeleteColor(color.id)}
             />
             <Button buttonType="edit" onClick={() => setEditMode(true)} />
-          </>
-
-          <ContrastChecker color={color} />
-          <h4>{color.role}</h4>
-          <p>contrast: {color.contrastText}</p>
+          </div>
         </>
       )}
     </div>
