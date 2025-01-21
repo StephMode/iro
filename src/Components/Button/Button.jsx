af9abb5b28c7a1c8b76c94d361aa5405a773f2cc
Stephan Model <stephan.model@outlook.de>
2025-01-21 18:21:02 +0100

    feat: change buttonLabel from strings to icons

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,18 +1,24 @@
 import PropTypes from "prop-types";
+import { IoTrash } from "react-icons/io5";
+import { IoBrush } from "react-icons/io5";
+import { IoAddCircle } from "react-icons/io5";
+import { IoCopyOutline } from "react-icons/io5";
+import { MdOutlineCancel } from "react-icons/md";
+import { IoIosSave } from "react-icons/io";
 
 export default function Button({ onClick, style, buttonType }) {
   const buttonTypeLabes = {
     labels: [
-      { labelType: "add", buttonLabel: "Add" },
-      { labelType: "edit", buttonLabel: "Edit" },
-      { labelType: "delete", buttonLabel: "Delete" },
-      { labelType: "save", buttonLabel: "Save" },
-      { labelType: "cancel", buttonLabel: "Cancel" },
-      { labelType: "copy", buttonLabel: "Copy" },
+      { labelType: "add", buttonLabel: <IoAddCircle /> },
+      { labelType: "edit", buttonLabel: <IoBrush /> },
+      { labelType: "delete", buttonLabel: <IoTrash /> },
+      { labelType: "save", buttonLabel: <IoIosSave /> },
+      { labelType: "cancel", buttonLabel: <MdOutlineCancel /> },
+      { labelType: "copy", buttonLabel: <IoCopyOutline /> },
     ],
     returnLabel() {
       const buttonLabelFinder = this.labels.find(
-        (label) => label.labelType === buttonType
+        (label) => label.labelType === buttonType,
       );
       return buttonLabelFinder ? buttonLabelFinder.buttonLabel : "";
     },
