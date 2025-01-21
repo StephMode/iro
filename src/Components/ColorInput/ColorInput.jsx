1936f5d2ac19a53203714ccad4aa9319eb1999fe
Stephan Model <stephan.model@outlook.de>
2025-01-21 08:54:34 +0100

    style: adjust indentation

--- src/Components/ColorInput/ColorInput.jsx
+++ src/Components/ColorInput/ColorInput.jsx
@@ -2,28 +2,27 @@ import { useState } from "react";
 
 export default function ColorInput({ id, defaultValue }) {
   const [inputValue, setInputValue] = useState(defaultValue);
- 
 
   const handleInputValue = (event) => {
     setInputValue(event.target.value);
   };
 
   return (
-    <div className="color-form--color-input-wrapper">    
-        <input
-          id={id}
-          name={id}
-          type="text"
-          value={inputValue}
-          onChange={handleInputValue}
-          className="color-form--input-field--color-text"
-        />
-        <input
-          type="color"
-          value={inputValue}
-          onChange={handleInputValue}
-          className="color-form--input-field--color-picker"
-        />
-      </div>
+    <div className="color-form--color-input-wrapper">
+      <input
+        id={id}
+        name={id}
+        type="text"
+        value={inputValue}
+        onChange={handleInputValue}
+        className="color-form--input-field--color-text"
+      />
+      <input
+        type="color"
+        value={inputValue}
+        onChange={handleInputValue}
+        className="color-form--input-field--color-picker"
+      />
+    </div>
   );
 }
